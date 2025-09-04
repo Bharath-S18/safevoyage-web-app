5b4c87700d77687ce2a3d84c0e01da248a74fa48
Gurucharan M <maripalagurucharan@gmail.com>
2025-09-04 18:10:08 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Dashboard';
+import DigitalID from './pages/DigitalID';
+import Alerts from './pages/Alert';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50 relative overflow-hidden">
+        {/* Grid Background */}
+        <div className="absolute inset-0 bg-grid-pattern opacity-5"></div>
+        <Navbar />
+        <main className="relative z-10">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/digital-id" element={<DigitalID />} />
+            <Route path="/alerts" element={<Alerts />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
